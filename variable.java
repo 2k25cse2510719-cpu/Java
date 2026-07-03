25530c5e7d63b0aa164e49c520038127af16f116
Ishan Chauhan <2k25cse2510719@gmail.com>
2026-07-03 09:39:28 +0530

    Learning variables

---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable{
+    public static void main(String[] args){
+        int age=21;
+        double money=2001.2;
+        char ch='A';
+        String str="Hello";
+boolean first=true;
+if(first){
+    System.out.println(age+" "+money+" "+ch+" "+str);
+}
+    }
+}
