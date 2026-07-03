529f0e2211499c200d7a9d6aae2eb7020d38bea5
Ishan Chauhan <2k25cse2510719@gmail.com>
2026-07-03 10:19:08 +0530

    Scanner

---
+++ scanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class scanner {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+System.out.println("Enter Your Name");
+  String name=sc.nextLine();
+System.out.println("Enter Your Age"); 
+        int age=sc.nextInt();
+System.out.println("Enter Your Salary");
+double salary=sc.nextDouble();
+System.out.println("Enter Your Grade");
+char grade=sc.next().charAt(0);
+System.out.println(name+" "+age+" "+salary+" "+grade);
+    }
+}
